7d2a0bb569a688a50286524780c0e13e1919b3f5
hganlee <msares123@gamil.com>
2022-10-08 16:37:48 +0900

    feat(CarName,ErrorMessage,CarNameTest): 자동차 이름에 대한 유효성 체크
    - 빈값 입력 채크
    - 길이 5자 이하 이름 채크

---
+++ src/main/java/racingcar/domain/ErrorMessage.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class ErrorMessage {
+    public static final String ERROR_PREFIX = "[ERROR]";
+
+    public static final String ERROR_CAR_NAME_EMPTY = formatErrorMessage("자동차 이름은 빈 값일 수 없습니다.");
+
+    public static final String ERROR_CAR_NAME_OVER_SIZE = formatErrorMessage("자동차 이름은 5자 이하만 가능합니다.");
+
+    private static String formatErrorMessage(String message) {
+        return String.format("%s %s", ERROR_PREFIX, message);
+    }
+}

de5110919e8fda77be13f2ca1e7e6a84a9fc2c81
hganlee <msares123@gamil.com>
2022-10-09 16:21:27 +0900

    feat(MoveNumber,MoveNumberTest): 전진 숫자 랜덤 생성
    - pickNumberInRange()를 활용해서 Random 값 추출
    - 0 에서 9 사이의 숫자 생성 채크

--- src/main/java/racingcar/common/ErrorMessage.java
+++ src/main/java/racingcar/common/ErrorMessage.java
@@ -2,10 +2,9 @@ package racingcar.common;
 
 public class ErrorMessage {
     public static final String ERROR_PREFIX = "[ERROR]";
-
     public static final String ERROR_CAR_NAME_EMPTY = formatErrorMessage("자동차 이름은 빈 값일 수 없습니다.");
-
     public static final String ERROR_CAR_NAME_OVER_SIZE = formatErrorMessage("자동차 이름은 5자 이하만 가능합니다.");
+    public static final String ERROR_MOVE_NUMBER_RANGE = formatErrorMessage("전진 숫자는 0 에서 9 사이의 숫자만 가능합니다.");
 
     private static String formatErrorMessage(String message) {
         return String.format("%s %s", ERROR_PREFIX, message);

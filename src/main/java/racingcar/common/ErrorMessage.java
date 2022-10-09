ba5accf022313df244ac323ffecf1bb17874c0d5
hganlee <msares123@gamil.com>
2022-10-09 17:28:40 +0900

    feat(PlayCount,PlayCountTest,ErrorMessage): 이동 횟수 객체 생성
    - 이동 횟수 빈값 입력 체크
    - 이동 횟수 양수만 입력 가능
    - 이동 횟수 최대값 2147483647 채크

--- src/main/java/racingcar/common/ErrorMessage.java
+++ src/main/java/racingcar/common/ErrorMessage.java
@@ -5,6 +5,9 @@ public class ErrorMessage {
     public static final String ERROR_CAR_NAME_EMPTY = formatErrorMessage("자동차 이름은 빈 값일 수 없습니다.");
     public static final String ERROR_CAR_NAME_OVER_SIZE = formatErrorMessage("자동차 이름은 5자 이하만 가능합니다.");
     public static final String ERROR_MOVE_NUMBER_RANGE = formatErrorMessage("전진 숫자는 0 에서 9 사이의 숫자만 가능합니다.");
+    public static final String ERROR_PLAYING_COUNT_EMPTY = formatErrorMessage("이동 횟수는 빈 값일 수 없습니다.");
+    public static final String ERROR_PLAYING_COUNT_RANGE = formatErrorMessage("이동 횟수는 양수만 입력 가능합니다.");
+    public static final String ERROR_PLAYING_COUNT_MAX = formatErrorMessage("이동 횟수의 최댓값은 2147483647 입니다.");
 
     private static String formatErrorMessage(String message) {
         return String.format("%s %s", ERROR_PREFIX, message);

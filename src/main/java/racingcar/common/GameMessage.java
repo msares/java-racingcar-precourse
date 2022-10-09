3849fef15284f4a1f40acd970798770b3ca239f6
hganlee <msares123@gamil.com>
2022-10-09 22:56:45 +0900

    feat(InputProvider,GameMessage): 입력 화면 구현

---
+++ src/main/java/racingcar/common/GameMessage.java
@@ -0,0 +1,6 @@
+package racingcar.common;
+
+public class GameMessage {
+    public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_PLAYING_COUNT = "시도할 횟수는 몇 회 인가요?";
+}
